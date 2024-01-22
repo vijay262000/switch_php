90ac110e8e0a5ea2c533c93600206764e2828ccf
vijay262000 <121922341+vijay262000@users.noreply.github.com>
2024-01-22 23:13:31 +0530

    Add files via upload

---
+++ switchcase.php
@@ -0,0 +1,27 @@
+<?php
+
+$age = 90;
+
+switch($age){
+
+    case 56:
+        echo"you age 56 years old";
+    break;
+
+    case 78:
+        echo"your age is 78 years old";
+    break;
+
+    case 80:
+        echo"your age is 80 years old";
+    break;
+
+    default: 
+        echo"your age is weird";
+    break;
+
+} 
+
+
+
+?>
